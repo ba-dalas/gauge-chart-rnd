a7eca42639f3d8a3acc4e39d666e3aaf7d0c2b7b
Rumi Jahan Dalas <rumi@bd.batworld.com>
2023-10-17 13:14:04 +0600

    gradient

--- src/app/gauge-chart-two/gauge-chart-two.component.ts
+++ src/app/gauge-chart-two/gauge-chart-two.component.ts
@@ -20,6 +20,40 @@ export class GaugeChartTwoComponent implements OnInit {
 
   createChart() {
 
+    const gaugeChartText ={
+      id:'gaugeChartText',
+      afterDatasetsDraw(chart:any, args:any , pluginsOption:any){
+       const {ctx , data , chartArea:{top , bottom , left , right , width , height},scales:{r} } = chart
+       ctx.save()
+      //  console.log(chart.getDatasetMeta(0).data[0].x)
+       const xCoor =chart.getDatasetMeta(0).data[0].x
+       const yCoor =chart.getDatasetMeta(0).data[0].y
+       const score =data.datasets[0].data[0]
+
+       ctx.fillRect(xCoor , yCoor, 400, 1)
+       ctx.font ='20px sans-serif'
+       ctx.fillStyle= '#666'
+       ctx.textBaseLine='top'
+
+       ctx.textAlign='left'
+       ctx.fillText('300', left , yCoor + 20)
+
+       ctx.textAlign='right'
+       ctx.fillText('850', right - 20 , yCoor + 20)
+
+       ctx.font ='150px sans-serif'
+       ctx.textAlign='center'
+       ctx.textBaseLine='bottom'
+       ctx.fillText(score, xCoor , yCoor )
+
+       ctx.font ='150px sans-serif'
+       ctx.textAlign='center'
+       ctx.textBaseLine='bottom'
+       ctx.fillText('Fair', xCoor , yCoor - 120 )
+      }
+    }
+
+
     this.chart = new Chart("MyChart", {
       type: "doughnut", //this denotes tha type of chart
 
@@ -27,7 +61,7 @@ export class GaugeChartTwoComponent implements OnInit {
         labels: ['Red', 'Pink', 'Green', 'Yellow', 'Orange', 'Blue',],
         datasets: [{
           label: 'My First Dataset',
-          data: [300, 240, 100, 432, 253, 34],
+          data: [650 , 200],
           backgroundColor: [
             'red',
             'pink',
@@ -36,19 +70,30 @@ export class GaugeChartTwoComponent implements OnInit {
             'orange',
             'blue',
           ],
-          hoverOffset: 4
+          hoverOffset: 4,
+
         }],
 
       },
       options: {
-        aspectRatio: 2.5,
+        aspectRatio: 1.5,
         cutout:'90%',
         circumference:180,
-        rotation:270
-      }
+        rotation:270,
+
+
+      },
+      plugins:[gaugeChartText]
+
 
     });
+
+
+
   }
+
+
+
 
 
 
